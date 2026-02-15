07acbb2e83e8c879f5073c52e99d28f04e68f355
Convidium <denyssianchuk0@gmail.com>
2026-02-15 14:14:24 +0100

    Did my first use of streams

---
+++ server.js
@@ -0,0 +1,45 @@
+import fastify from "fastify";
+import fs from "node:fs"
+import path from "node:path"
+import { pipeline } from "node:stream/promises";
+
+/* This approach is not perfect. In this particular case, it works fine, since our readStream and writeStream have exact same characteristics. 
+They can process same amount of data, so there's no problem with so called "backpressuring". 
+But if we read from SSD, and write to HDD, our HDD can't process the incoming chunks of data as fast as an SSD sends it, so there is a problem? */
+
+/* createReadStream creates a "bridge" between a file, or any other space of disk memory, and the RAM. By default, the size of allocated RAM space is 64KB.
+readStream.on() allows us to transfer data from disk space to RAM, by tansfering data, split at "chunks". Each chunk is put at exactly the allocated RAM space,
+until a certain operation is done at it, so the garbage collector can put it away (delete it?). 
+In our case, we log the buffer into the console, and then send that buffer to writeStream pipe. 
+*/
+
+/* createWriteStream does bascally the same thing, but it creates a bridge from RAM to disk space, that we select, when creating this pipe.
+We recieve buffer from a memory, and send it to be written at certain disk space. For that we use writeStream.write().
+
+The issues occur, when writeStream writes data slower than the readStream recieves it and sends it to writeStream.
+If readStream sends buffer chunk to writeStream, but it is still busy with the previous chunk - it leaves it in RAM, 
+and so we now have a waitlist of buffer chunks to be sent via writeStream.
+And what happens if we read from a file way faster than we write to the other file? And the initial file size is way larger than what RAM could contain?
+Well, as we place chunk and another chunk in RAM, at some point or RAM is full of chunks. We now don't have a place to store it in memory.
+This is Heap out of Memory error.  
+*/
+
+const main = async () => {
+    const readStream = fs.createReadStream("./test/customers.csv");
+
+    const writeStream = fs.createWriteStream("./test/exported-customers.csv");
+
+    readStream.on("data", (buffer) => {
+        console.log("Data:\n", buffer.toString())
+
+        writeStream.write(buffer)
+    })
+
+    readStream.on("end", () => {
+        console.log("Stream ended");
+
+        writeStream.end();
+    })
+}
+
+main();
