b3ce7174e85e650f17327416f32ab46b6907c650
Convidium <denyssianchuk0@gmail.com>
2026-02-18 22:10:53 +0100

    Added error detection, separated custom streams

--- src/02-csv-to-json/csv-to-json.js
+++ src/02-csv-to-json/csv-to-json.js
@@ -1,92 +1,13 @@
 import fs from "node:fs"
-import { Transform } from "node:stream";
 import { pipeline } from "node:stream/promises";
+import { csvToObjectStream, ageFilterStream, JSONStringifyStream } from "./custom-streams.js";
 
 const main = async () => {
-    const readStream = fs.createReadStream("data/output/random.csv");
-    const writeStream = fs.createWriteStream("data/output/clean.json");
+    const INPUT = "data/output/random.csv";
+    const OUTPUT = "data/output/clean.json";
 
-    const csvToObjectStream = new Transform({
-        readableObjectMode: true,
-        writableObjectMode: true,
-
-        construct(callback) {
-            this.remainder = "";
-            this.headers = null;
-            callback();
-        },
-
-        transform(chunk, encoding, callback) {
-            const data = this.remainder + chunk.toString();
-            const lines = data.split("\n");
-
-            this.remainder = lines.pop();
-
-            lines.forEach((line) => {
-                const values = line.split(",");
-
-                if (!this.headers) {
-                    this.headers = values;
-                    return;
-                }
-
-                if (values.length === this.headers.length) {
-                    const obj = {};
-                    this.headers.forEach((header, index) => {
-                        obj[header.trim()] = values[index].trim();
-                    });
-
-                    this.push(obj);
-                }
-            });
-            callback();
-        }
-    });
-
-    const ageFilterStream = new Transform({
-        readableObjectMode: true,
-        writableObjectMode: true,
-
-        construct(callback) {
-            this.minAge = 18;
-            callback();
-        },
-
-        transform(user, encoding, callback) {
-            const userAge = parseInt(user.age);
-            if (userAge >= this.minAge) {
-                this.push(user);
-            }
-            callback();
-        }
-    });
-
-    const JSONStringifyStream = new Transform({
-        readableObjectMode: true,
-        writableObjectMode: true,
-
-        construct(callback) {
-            this.isFirstObject = true;
-            callback();
-        },
-
-        transform(user, encoding, callback) {
-            const jsonString = JSON.stringify(user, null, 2);
-                    if (this.isFirstObject) {
-                        this.push('[\n  ' + jsonString);
-                        this.isFirstObject = false;
-                    } else {
-                        this.push(',\n  ' + jsonString);
-                    }
-            callback();
-        },
-
-        flush(callback) {
-            this.push("\n]")
-            callback();
-        }
-        
-    });
+    const readStream = fs.createReadStream(INPUT);
+    const writeStream = fs.createWriteStream(OUTPUT);
 
     try {
         await pipeline(
@@ -96,11 +17,19 @@ const main = async () => {
             JSONStringifyStream,
             writeStream
         );
-    } catch (err) {
-        console.log("Whoa! There's an error:", err);
-    }
 
-    console.log("Finished");
+        console.log("Finished succesfully!");
+    } catch (err) {
+        console.log("Critical error detected:");
+
+        if (err.code == "EACCES") {
+            console.error("Problem with file access. A file could not be modified.");
+        } else if (err.code == "ENOENT") {
+            console.error("Folder doesn't exist.");
+        } else {
+            console.error("Unknown error.");
+        }
+    }
 }
 
 main();
---
+++ src/02-csv-to-json/custom-streams.js
@@ -0,0 +1,77 @@
+import { Transform } from "node:stream";
+
+export const csvToObjectStream = new Transform({
+    readableObjectMode: true,
+    writableObjectMode: true,
+
+    construct(callback) {
+        this.remainder = "";
+        this.headers = null;
+        callback();
+    },
+
+    transform(chunk, encoding, callback) {
+        const data = this.remainder + chunk.toString();
+        const lines = data.split("\n");
+        this.remainder = lines.pop();
+
+        lines.forEach((line) => {
+            const values = line.split(",");
+            if (!this.headers) {
+                this.headers = values;
+                return;
+            }
+            if (values.length === this.headers.length) {
+                const obj = {};
+                this.headers.forEach((header, index) => {
+                    obj[header.trim()] = values[index].trim();
+                });
+                this.push(obj);
+            }
+        });
+        callback();
+    }
+});
+
+export const ageFilterStream = new Transform({
+    readableObjectMode: true,
+    writableObjectMode: true,
+
+    construct(callback) {
+        this.minAge = 18;
+        callback();
+    },
+
+    transform(user, encoding, callback) {
+        const userAge = parseInt(user.age);
+        if (userAge >= this.minAge) {
+            this.push(user);
+        }
+        callback();
+    }
+});
+
+export const JSONStringifyStream = new Transform({
+    readableObjectMode: true,
+    writableObjectMode: true,
+    construct(callback) {
+        this.isFirstObject = true;
+        callback();
+    },
+
+    transform(user, encoding, callback) {
+        const jsonString = JSON.stringify(user, null, 2);
+                if (this.isFirstObject) {
+                    this.push('[\n  ' + jsonString);
+                    this.isFirstObject = false;
+                } else {
+                    this.push(',\n  ' + jsonString);
+                }
+        callback();
+    },
+
+    flush(callback) {
+        this.push("\n]")
+        callback();
+    }
+});
